b63b5c6ee55e0894e753cb84132b2b81bb963b2c
hainguyen-eh <nguyenngochai.shipagent@gmail.com>
2025-06-14 04:45:50 +0700

    Add basic home page

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root "home#index"
+
   resources :users, only: [ :index ]
+  resources :books
 end
